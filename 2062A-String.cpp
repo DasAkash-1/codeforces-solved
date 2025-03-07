05074594196fbdb3251f7b1c48a340077485f200
DasAkash-1 <142176252+DasAkash-1@users.noreply.github.com>
2025-03-07 19:09:16 +0600

    Add files via upload

---
+++ 2062A-String.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        string s;
+        cin >> s;
+        int count = 0;
+
+        for (int i = 0; i < s.length(); i++)
+        {
+            if (s[i] == '1')
+            {
+                count++;
+            }
+            
+        }
+        cout << count << '\n';
+    }
+    
+}
