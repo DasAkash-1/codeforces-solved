83c83a501d956b1629f6376b2ebea9b5a9793028
DasAkash-1 <142176252+DasAkash-1@users.noreply.github.com>
2025-04-24 02:58:56 +0600

    Add files via upload

---
+++ 2086A-Cloudberryjam.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int t;
+    cin >> t;
+    
+    while(t--){
+        int j, b = 2;
+        cin >> j;
+        int sum = j * b;
+        cout << sum << endl;
+    }
+}
